92579d0477899079a5cd981ce79156fb5b35b173
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-05-26 22:41:08 +0900

    구구단 프로그램

---
+++ src/GuGu.java
@@ -0,0 +1,19 @@
+public class GuGu {
+    public void dan(int n){
+        for(int i=1;i<10;i++){
+            System.out.println(n*i);
+        }
+
+    }
+    public static void main(String[] args){
+        GuGu gugu = new GuGu();
+        gugu.dan(2);
+        gugu.dan(3);
+        gugu.dan(4);
+        gugu.dan(5);
+        gugu.dan(6);
+        gugu.dan(7);
+        gugu.dan(8);
+        gugu.dan(9);
+    }
+}
