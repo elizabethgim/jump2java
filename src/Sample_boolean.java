6af6884400d5d986c90f528c76c5c7c145889d6b
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-05-29 19:53:18 +0900

    자바 boolean 사용하기

---
+++ src/Sample_boolean.java
@@ -0,0 +1,16 @@
+public class Sample_boolean {
+    public static void main(String[] args){
+        int base = 180;
+        int height = 185;
+        boolean isTall = height > base;
+
+        if (isTall) {
+            System.out.println("Tall");
+        }
+
+        int i = 3;
+        boolean isOdd = i % 2 == 1;
+        System.out.println(isOdd);
+    }
+
+}
