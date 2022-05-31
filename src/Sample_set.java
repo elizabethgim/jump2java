b8cf7b42f1e3cc8c4ab3eeb44eafb3d359e2912b
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-05-31 22:16:09 +0900

    set 값 추가하기, 삭제하기, 연산(교집합, 합집합, 차집합)

--- src/Sample_set.java
+++ src/Sample_set.java
@@ -5,5 +5,35 @@ public class Sample_set {
     public static void main(String[] args) {
         HashSet<String> set = new HashSet<>(Arrays.asList("H", "e", "l", "l", "o"));
         System.out.println(set); // [e, H, l, o]
+
+        HashSet<Integer> s1 = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        HashSet<Integer> s2 = new HashSet<>(Arrays.asList(4, 5, 6, 7, 8, 9));
+
+        // 교집합
+        HashSet<Integer> intersection = new HashSet<>(s1);
+        intersection.retainAll(s2);
+        System.out.println(intersection);
+
+        // 합집합
+        HashSet<Integer> union = new HashSet<>(s1);
+        union.addAll(s2);
+        System.out.println(union);
+
+        // 차집합
+        HashSet<Integer> substract = new HashSet<>(s1);
+        substract.removeAll(s2);
+        System.out.println(substract);
+
+        // 값 추가하기
+        HashSet<String> set3 = new HashSet<>();
+        set3.add("Jump");
+        set3.add("To");
+        set3.add("Java");
+        System.out.println(set3);  // [Java, To, Jump] 출력
+
+        // 값 삭제하기
+        HashSet<String> set4 = new HashSet<>(Arrays.asList("Jump", "To", "Java"));
+        set.remove("To");
+        System.out.println(set4);  // [Java, Jump] 출력
     }
 }
