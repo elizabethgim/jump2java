e20b6a2c1dcbb513127dd9988c2355a8113039bc
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-06-01 23:10:36 +0900

    treeset 사용하기

---
+++ src/Sample_TreeSet.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.TreeSet;
+
+public class Sample_TreeSet {
+    public static void main(String[] args){
+        TreeSet <Integer> set = new TreeSet <Integer>(Arrays.asList(4,2,3)); // 초기값 지정
+        System.out.println(set); // 전체출력 [2,3,4]
+        System.out.println("최소값: " + set.first()); // 최소값 출력
+        System.out.println("최대값: " + set.last()); // 최대값 출력
+        System.out.println("3보다 작은값: " + set.higher(3)); // 입력값보다 큰 데이터중 최소값 출력 없으면 null
+        System.out.println("3보다 큰 값: " + set.lower(3)); // 입력값보다 작은 데이터중 최대값 출력 없으면 null
+
+
+    }
+
+}
