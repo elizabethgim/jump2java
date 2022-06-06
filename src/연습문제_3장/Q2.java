e8177a88b6814c9d8d29d0ba76bbc6ccae42d0a9
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-06-06 20:32:28 +0900

    3장 연습문제 2번: 자연수 13이 홀수인지 짝수인지 판별할 수 있는 방법에 대해 말해 보자.

---
+++ src/연습문제_3장/Q2.java
@@ -0,0 +1,16 @@
+package 연습문제_3장;
+
+public class Q2 {
+    public static String checkNum(int num){
+        if(num % 2 == 0){
+            return "짝수";
+        } else{
+            return  "홀수";
+        }
+    }
+
+    public static void main(String[] args){
+//        System.out.println(checkNum(2));
+        System.out.println(checkNum(13));
+    }
+}
