75f514bf174e90d5d69fd665f8685a42a90d6e1a
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-07-05 23:40:52 +0900

    Console input output

---
+++ src/Sample_String.java
@@ -0,0 +1,7 @@
+public class Sample_String {
+    String a = "hello";
+    String b = "java";
+    String c = "hello";
+    System.out.println(a.equals(b)); // false 출력
+    System.out.println(a.equals(c)); // true 출력
+}
