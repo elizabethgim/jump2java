fbfd3358864446a10fe4a190d2597142912d160e
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-07-20 22:24:05 +0900

    Q5

---
+++ src/Quiz03/Q5.java
@@ -0,0 +1,9 @@
+package Quiz03;
+
+public class Q5 {
+    public static void main(String[] args) {
+        String a = "a:b:c:d";
+        a = a.replace(":", "#");
+        System.out.println(a);
+    }
+}
