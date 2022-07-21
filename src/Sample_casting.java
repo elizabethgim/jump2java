946dfbb88bcbc7ba05fc083d1cea9a7390a7cd77
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-07-21 20:45:30 +0900

    형변환

---
+++ src/Sample_casting.java
@@ -0,0 +1,7 @@
+public class Sample_casting {
+    public static void main(String[] args){
+        String num = "123";
+        int n = Integer.parseInt(num);
+        System.out.println(n);
+    }
+}
