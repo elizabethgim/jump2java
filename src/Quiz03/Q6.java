c7c0fc1fe46eda7d2f5e8d191fc89d7f9ccdf001
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-07-24 20:58:48 +0900

    Comparator

---
+++ src/Quiz03/Q6.java
@@ -0,0 +1,12 @@
+package Quiz03;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Q6 {
+    public static void main(String[] args) {
+        ArrayList<Integer> myList = new ArrayList<>(Arrays.asList(1, 3, 5, 4, 2));
+        myList.sort(Comparator.reverseOrder());
+        System.out.println(myList); // [1, 3, 5, 4, 2] 출력
+    }
+}
