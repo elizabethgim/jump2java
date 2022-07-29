d219968a4c80de47f1ce9d6171349e013ecba2fb
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-07-30 00:39:48 +0900

    프로그램 개선 : Interger 매직넘버 사용 -> enum 사용

---
+++ src/Quiz03/Q10.java
@@ -0,0 +1,24 @@
+package Quiz03;
+
+import java.util.HashMap;
+enum CoffeeType{
+    AMERICANO,
+    ICE_AMERICANO,
+    CAFE_LATTE
+};
+
+public class Q10 {
+    static void printCoffeePrice(CoffeeType type) {
+        HashMap<CoffeeType, Integer> priceMap = new HashMap<>();
+        priceMap.put(CoffeeType.AMERICANO, 3000);  // 1: 아메리카노
+        priceMap.put(CoffeeType.ICE_AMERICANO, 4000);  // 2: 아이스 아메리카노
+        priceMap.put(CoffeeType.CAFE_LATTE, 5000);  // 3: 카페라떼
+        int price = priceMap.get(type);
+        System.out.println(String.format("가격은 %d원 입니다.", price));
+    }
+
+    public static void main(String[] args) {
+        printCoffeePrice(CoffeeType.AMERICANO);  // "가격은 3000원 입니다." 출력
+//        printCoffeePrice(99);  // NullPointerException 발생
+    }
+}
