a2e78cca1c4e984d3ae19134b6a71f8b812b1ddc
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-07-30 19:27:42 +0900

    if else 구문

---
+++ src/Ch04_control/Sample.java
@@ -0,0 +1,12 @@
+package Ch04_control;
+
+public class Sample {
+    public static void main(String[] args){
+        boolean money = true;
+        if (money) {
+            System.out.println("택시를 타고 가라");
+        }else {
+            System.out.println("걸어가라");
+        }
+    }
+}
