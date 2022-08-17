ce3ec15c068708ffe1ee0a63b4b9dd2e097b6160
elizabethgim <yujingim@kookmin.ac.kr>
2022-08-17 15:48:37 +0900

    정답!

---
+++ src/Ch04_control/Quiz/Q3.java
@@ -0,0 +1,12 @@
+package Ch04_control.Quiz;
+
+public class Q3 {
+    public static void main(String[] args){
+        for(int i=0;i<5;i++){
+            for(int j=i+1;j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
