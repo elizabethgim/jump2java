069044c1e8e42e75ee8157da88a0c2a663e7530b
elizabethgim <yujingim@kookmin.ac.kr>
2022-08-18 21:24:15 +0900

    정답!

---
+++ src/Ch04_control/Quiz/Q4.java
@@ -0,0 +1,9 @@
+package Ch04_control.Quiz;
+
+public class Q4 {
+    public static void main(String[] args){
+        for(int i=0;i<101;i++){
+            System.out.println(i);
+        }
+    }
+}
