2444a055c9caf8ca9e9e9c737ecbeb6a8d11b858
elizabethgim <yujingim@kookmin.ac.kr>
2022-09-10 10:39:58 +0900

    default 접근제어자 Subpackage

---
+++ src/house/person/YujinGim.java
@@ -0,0 +1,9 @@
+package house.person;
+import house.HouseKim;
+
+public class YujinGim extends HouseKim{
+    public static void main(String[] args){
+        YujinGim yg = new YujinGim();
+        System.out.println(yg);
+    }
+}
